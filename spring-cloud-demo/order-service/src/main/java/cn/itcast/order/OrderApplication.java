0c26e99d42a10d1bf23e55c9ec0353a97341df51
Lanqilu <885240677@qq.com>
2021-09-09 17:27:28 +0800

    Ribbon 负载均衡

--- spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -20,7 +20,7 @@ public class OrderApplication {
      */
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
