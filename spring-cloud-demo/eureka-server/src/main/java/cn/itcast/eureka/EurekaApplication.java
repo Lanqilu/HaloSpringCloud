6485893c857e8407573b163f91fd7b6b7cfd4cfe
Lanqilu <885240677@qq.com>
2021-09-09 15:48:31 +0800

    搭建 eureka-server

---
+++ spring-cloud-demo/eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Halo
+ * @create 2021/09/09 下午 03:28
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
