8a331e3bd7b15c8eefa4de64867832885de80e29
Lanqilu <885240677@qq.com>
2021-09-10 12:15:10 +0800

    使用 Feign 替代 RestTemplate

--- spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,24 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
-    }
-
-    /**
-     * 创建 RestTemplate 并注入容器
-     */
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
 }
