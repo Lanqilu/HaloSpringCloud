95676f212554287332e76f7cba07e5fce2db77f7
Lanqilu <885240677@qq.com>
2021-09-10 14:03:42 +0800

    GateWay 过滤器工厂

---
+++ spring-cloud-demo/gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Halo
+ * @create 2021/09/10 下午 01:17
+ * @description
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
--- spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,11 +1,12 @@
 package cn.itcast.order;
 
+import cn.itcast.feign.clients.UserClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "cn.itcast.feign.clients")
+@EnableFeignClients(clients = UserClient.class)
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
