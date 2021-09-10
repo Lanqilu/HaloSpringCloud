8b92adb317416afb62a7259eeea790c05f9e2c1c
Lanqilu <885240677@qq.com>
2021-09-10 13:03:33 +0800

    基于抽取的 Feign 最佳实践实现

--- spring-cloud-demo/feign-api/src/main/java/cn/itcast/feign/config/DefaultFeignConfiguration.java
+++ spring-cloud-demo/feign-api/src/main/java/cn/itcast/feign/config/DefaultFeignConfiguration.java
@@ -1,16 +1,16 @@
-package cn.itcast.order.config;
+package cn.itcast.feign.config;
 
 import feign.Logger;
 import org.springframework.context.annotation.Bean;
 
 /**
  * @author Halo
- * @create 2021/09/10 下午 12:26
+ * @create 2021/09/10 下午 12:55
  * @description
  */
-public class DefaultFeignConfiguration  {
+public class DefaultFeignConfiguration {
     @Bean
-    public Logger.Level feignLogLevel(){
+    public Logger.Level feignLogLevel() {
         return Logger.Level.BASIC; // 日志级别为BASIC
     }
 }
--- spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ spring-cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,12 +3,9 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "cn.itcast.feign.clients")
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
